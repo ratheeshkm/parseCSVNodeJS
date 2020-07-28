b64a04984b8cacd7fe824eb42084fb68e2ce3eb7
Ratheesh <ratkumar@linkedin.com>
2020-07-28 14:08:24 +0530

    Change

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const userRoutes = require("./users");
 
 const appRouter = (app, fs) => {
   app.get("/", (req, res) => {
-    res.send("Welcome to the development server");
+    res.send("Welcome to the development server!");
   });
 
   userRoutes(app, fs);
