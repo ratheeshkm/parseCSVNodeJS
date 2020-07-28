76d02517b9e0abb45128bbb799dcff60d2b0c8fc
Ratheesh <ratkumar@linkedin.com>
2020-07-28 14:25:26 +0530

    CSV to JSON

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,21 @@
+const csv = require("csvtojson");
+const csvFilePath = "./data/TestData8.csv";
+
 const userRoutes = (app, fs) => {
   const dataPath = "./data/dummyData.json";
+
   app.get("/users", (req, res) => {
+    /*
     fs.readFile(dataPath, "utf8", (err, data) => {
       if (err) throw err;
       res.send(JSON.parse(data));
-    });
+		});
+		*/
+    csv()
+      .fromFile(csvFilePath)
+      .then((jsonObj) => {
+        res.send(jsonObj);
+      });
   });
 };
 
